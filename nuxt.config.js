9bf93e6e38a9fcc4ffd6357be3745ba1d173b17b
SilentGRV <katsu.kbc.91@gmail.com>
2018-03-01 10:08:28 +0900

    test commit

---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s - Nuxt.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    modules: [
+      'bootstrap-vue/nuxt',
+      // Or if you have custom bootstrap CSS...
+      ['bootstrap-vue/nuxt', { css: false }],
+      
+      // Simple usage
+      '@nuxtjs/bootstrap-vue',
+
+      // With options
+      ['@nuxtjs/bootstrap-vue', { css: false }],
+      '@nuxtjs/font-awesome',
+    ]
+  },
+
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
